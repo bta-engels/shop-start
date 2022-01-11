7cf0c8c8f2f1869e9669d68045d609a94612f3c4
berndengels <engels@f50.de>
2022-01-11 11:38:47 +0100

    stuff

--- controllers/ManufacturersController.php
+++ controllers/ManufacturersController.php
@@ -1,8 +1,13 @@
 <?php
-
+require_once 'models/Model.php';
+;
 class ManufacturersController {
 
     public function index() {
+        $db = new Model;
+        $sql = 'SELECT * FROM manufacturers ORDER BY name';
+        $data = $db->getAll($sql);
+
         require_once 'views/public/manufacturers/index.php';
     }
 }
--- index.php
+++ index.php
@@ -11,9 +11,21 @@ if( isset($_GET['controller']) ) {
     switch($_GET['controller']) {
         case 'manufacturers':
             // ManufacturersController importieren und als $controller instanzieren
+            require_once 'controllers/ManufacturersController.php';
+            $controller = new ManufacturersController();
+            break;
+        case 'products':
+            // ProductsController importieren und als $controller instanzieren
+            require_once 'controllers/ProductsController.php';
+            $controller = new ProductsController();
             break;
     }
     // hier action abfragen
+    if( isset($_GET['action']) && $controller && method_exists($controller, $_GET['action']) ) {
+        $action = $_GET['action'];
+        $controller->$action();
+    }
+
 } else {
     // zeige hier start seite
 }
