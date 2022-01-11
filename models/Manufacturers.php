82d6732b3f336eada6d953700d3ea6484726da16
bakrihamza <bakrihamza2209@gmail.com>
2022-01-11 12:24:44 +0100

    stuff

---
+++ models/Manufacturers.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'models/Model.php';
+
+class Manufacturers extends Model{
+
+    public function all() {
+        $sql = "SELECT * FROM manufacturers ORDER BY name";
+        return $this->getAll($sql);
+    }
+
+}
