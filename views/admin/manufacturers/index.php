1712b345dfdb643a4354682bc67a9ec073957cf5
berndengels <engels@f50.de>
2022-01-12 18:59:03 +0100

    stuff

--- views/admin/manufacturers/index.php
+++ views/admin/manufacturers/index.php
@@ -8,9 +8,9 @@
         <h3 class="title"><?php echo $item['name']; ?></h3>
         <p ><?php echo $item['description']; ?></p>
         <div class="manubuttons">
-            <a href="/manufacturers/<?php echo $item['id'] ?>">Details<i class="fas fa-angle-double-right"></i></a>
-            <a href="/manufacturers/edit/<?php echo $item['id'] ?>">Edit<i class="fas fa-angle-double-right"></i></a>
-            <a href="/manufacturers/delete/<?php echo $item['id'] ?>">Delete<i class="fas fa-angle-double-right"></i></a>
+            <a href="/manufacturers/<?php echo $item['id'] ?>"><i class="fas fa-angle-double-right"></i> Details</a>
+            <a href="/manufacturers/edit/<?php echo $item['id'] ?>"><i class="fas fa-edit"></i> Edit</a>
+            <a href="/manufacturers/delete/<?php echo $item['id'] ?>"><i class="fas fa-trash-alt"></i> Delete</a>
         </div>
     </div>
     <?php endforeach; ?>
