e2881770b3eb421fa4addd743d8874d5273bf1fc
berndengels <engels@f50.de>
2022-01-12 13:55:35 +0100

    stuff

---
+++ views/admin/products/index.php
@@ -0,0 +1,16 @@
+
+<div class="containerBox">
+    <?php foreach ($data as $item):?>
+        
+    
+    <div class="box">
+        <div class="icon">
+            <span class="fas fa-code"></span>
+        </div>
+        <h3 class="title"><?php echo $item['name']; ?></h3>
+        <p ><?php echo $item['description']; ?></p>
+        <a href="/products/<?php echo $item['id'] ?>">read more <i class="fas fa-angle-double-right"></i></a>
+    </div>
+    <?php endforeach;?>
+
+</div>
