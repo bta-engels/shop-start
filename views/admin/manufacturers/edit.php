11e5eaf658e042f84f85adf3f156f9075f19ac92
berndengels <engels@f50.de>
2022-01-13 08:51:34 +0100

    soleil

--- views/admin/manufacturers/edit.php
+++ views/admin/manufacturers/edit.php
@@ -3,21 +3,20 @@ require_once 'inc/header.php';
 
 ?>
 <div class="editBox">
-<form  action="" method="post">
-                <h2>Update </h2>
-                <label >Manufacturer Name</label><br>
-                <input type="text" name="name" class="field"   placeholder="Name">
-                <label >Manufacturer Description</label><br>
-                <div class="select">
-                    <select name="" id="">
-                        <option value="">item</option>
-                        <option value="">item</option>
-
-                    </select>
-                </div>
-                <textarea name="description" class="field"  cols="30" rows="10"  placeholder="Description"></textarea>
-                <button class="btn">Update</button>
-            </form>
+    <form  action="" method="post">
+        <h2>Update </h2>
+        <label >Manufacturer Name</label><br>
+        <input type="text" name="name" class="field"   placeholder="Name">
+        <label >Manufacturer Description</label><br>
+        <div class="select">
+            <select name="" id="">
+                <option value="">item</option>
+                <option value="">item</option>
+            </select>
+        </div>
+        <textarea name="description" class="field"  cols="30" rows="10"  placeholder="Description"></textarea>
+        <button class="btn">Update</button>
+    </form>
 </div>
 
 <?php
