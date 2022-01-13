392f84368c2f935aa8793ebe7748a13db1188e00
berndengels <engels@f50.de>
2022-01-13 11:02:41 +0100

    stuff

--- views/admin/manufacturers/edit.php
+++ views/admin/manufacturers/edit.php
@@ -5,7 +5,7 @@ require_once 'inc/header.php';
     <form  action="/manufacturers/store<?php if(isset($data['id'])) echo '/'.$data['id']; ?>" method="post">
         <h2>Hersteller</h2>
         <label >Manufacturer Name</label><br>
-        <input type="text" name="name" class="field"  value="<?php echo $data['name'] ?? ''; ?>" placeholder="Name">
+        <input type="text" name="name" class="field" value="<?php echo $data['name'] ?? ''; ?>" placeholder="Name">
         <label >Manufacturer Description</label><br>
         <textarea name="description" class="field" cols="30" rows="10" 
             placeholder="Description" ><?php echo $data['description'] ?? ''; ?></textarea>
